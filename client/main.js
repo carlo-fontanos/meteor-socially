b88037834f8c358803ab651e7687cfd0daab6463
Carlo Fontanos <carl.fontanos@gmail.com>
2016-11-08 14:35:12 +0800

    Added a simple dynamic app that feratures separate model view and controller on CLIENT side.

--- client/main.js
+++ client/main.js
@@ -3,4 +3,28 @@ import angularMeteor from 'angular-meteor';
  
 angular.module('socially', [
 	angularMeteor
-]);
+])
+.controller('PartiesListCtrl', function($scope) {
+	'ngInject';
+	
+	$scope.parties = [{
+		'name': 'Dubstep-Free Zone',
+		'description': 'Can we please just for an evening not listen to dubstep.'
+	}, {
+		'name': 'All dubstep all the time',
+		'description': 'Get it on!'
+	}, {
+		'name': 'Savage lounging',
+		'description': 'Leisure suit required. And only fiercest manners.'
+	}];
+});
+
+function onReady() {
+	angular.bootstrap(document, ['socially']);
+}
+
+if (Meteor.isCordova) {
+	angular.element(document).on('deviceready', onReady);
+} else {
+	angular.element(document).ready(onReady);
+}
